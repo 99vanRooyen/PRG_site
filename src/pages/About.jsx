8188520e165390f9672f656d02fc010f5f75db2c
Leonard van Rooyen <99.vanRooyen@gmail.com>
2025-12-19 10:56:16 +0200

    Fix Navbar import casing across pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 import AboutUs from "../components/aboutUs"
-import Navbar from "../components/navbar"
+import Navbar from "../components/Navbar"
 import Footer from "../components/footer"
 
 function About() {
