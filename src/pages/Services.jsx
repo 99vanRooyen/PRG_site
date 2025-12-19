9b4c0cd38283b2d6a37c9d0c0f805d87602d2e01
Leonard van Rooyen <99.vanRooyen@gmail.com>
2025-12-19 12:41:11 +0200

    Fix Services background image for Netlify build

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -5,17 +5,25 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/footer";
 import useScrollReveal from "../hooks/useScrollReveal";
 
+// ✅ Import background image so Vite bundles it correctly (works on Netlify)
+import homeBg from "../assets/Home-Bg.jpg"; // make sure this matches your actual filename + extension + casing
+
 function Services() {
   useScrollReveal();
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center bg-[url('./assets/Home-Bg.jpg')] bg-repeat bg-center text-black">
+      <div
+        className="flex flex-col items-center justify-center bg-repeat bg-center text-black"
+        style={{ backgroundImage: `url(${homeBg})` }}
+      >
         {/* Navbar component */}
         <Navbar />
 
         <h1 className="text-center text-5xl font-extrabold py-6 tracking-wide">
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#9fb658] to-[#545f2c]">SERVICES</span>
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#9fb658] to-[#545f2c]">
+            SERVICES
+          </span>
         </h1>
 
         {/* Large Format Printing component */}
@@ -28,12 +36,12 @@ function Services() {
 
         {/* Promotional Printing component */}
         <Promotional_Printing />
-        
+
         {/* Footer component */}
         <Footer />
       </div>
     </>
-  )
+  );
 }
 
-export default Services
+export default Services;
